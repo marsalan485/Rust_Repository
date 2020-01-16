09505183b0a1c928ba8686859ed49ccf57970b51
your username <marsalan485@gmail.com>
2020-01-16 12:50:59 +0500

    My second commit.

---
+++ Counting_Crate/target/package/Counting_Crate-0.1.0/target/debug/deps/Counting_Crate-77d6cba4fd35eb12.d
@@ -0,0 +1,7 @@
+/home/arsalan/Desktop/My_Repository/Rust_Repository/Counting_Crate/target/package/Counting_Crate-0.1.0/target/debug/deps/Counting_Crate-77d6cba4fd35eb12.rmeta: src/lib.rs
+
+/home/arsalan/Desktop/My_Repository/Rust_Repository/Counting_Crate/target/package/Counting_Crate-0.1.0/target/debug/deps/libCounting_Crate-77d6cba4fd35eb12.rlib: src/lib.rs
+
+/home/arsalan/Desktop/My_Repository/Rust_Repository/Counting_Crate/target/package/Counting_Crate-0.1.0/target/debug/deps/Counting_Crate-77d6cba4fd35eb12.d: src/lib.rs
+
+src/lib.rs:
